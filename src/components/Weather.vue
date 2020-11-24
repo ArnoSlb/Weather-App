e12e7148824afac03d86d0cc5f208256857ec778
ArnoSlb <soulabaille.arnaud@gmail.com>
2020-11-24 15:08:58 +0100

    ajout des aurores et crepuscules, version responsive to do

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -13,55 +13,58 @@
             >
                 <div>
                     <h3>Détails de la météo pour {{weather.name}}</h3>
-                    <h4 class=weather-description>{{weather.weather[0].description}}</h4>
+                    <h4 class="weather-description">{{weather.weather[0].description}}</h4>
+                    <h4 class="categories">Les températures</h4>
                     <div class="details-temp-box">
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
                             <p class="details-text">min</p>
-                            <p class="thermometer-number">{{weather.main.temp_min.toFixed()}}°C</p>
+                            <p class="thermometer-number">{{weather.main.temp_min.toFixed()}} °C</p>
                         </div>
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
-                            <p class="thermometer-number">{{weather.main.temp.toFixed()}}°C</p>
+                            <p class="thermometer-number">{{weather.main.temp.toFixed()}} °C</p>
                         </div> 
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
                             <p class="details-text">ressentis</p>
-                            <p class="thermometer-number">{{weather.main.feels_like.toFixed()}}°C</p>
+                            <p class="thermometer-number">{{weather.main.feels_like.toFixed()}} °C</p>
                         </div>  
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
                             <p class="details-text">max</p>
-                            <p class="thermometer-number">{{weather.main.temp_max.toFixed()}}°C</p>
+                            <p class="thermometer-number">{{weather.main.temp_max.toFixed()}} °C</p>
                         </div>   
                     </div>
+                    <h4 class="categories">Anémométrie</h4>
                     <div class="details-temp-box">  
                         <div class="details-infos-box">
                             <img class="humidity-icon" src="../assets/weather-pack-icon/humidity.png" alt="">
                             <p class="details-text">humidité</p>
-                            <p class="thermometer-number">{{weather.main.humidity.toFixed()}}%</p>
+                            <p class="thermometer-number">{{weather.main.humidity.toFixed()}} %</p>
                         </div>   
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/027-wind-sign.png" alt="">
                             <p class="details-text">vitesse</p>
-                            <p class="thermometer-number">{{weather.wind.speed}}km/h</p>
+                            <p class="thermometer-number">{{weather.wind.speed}} km/h</p>
                         </div>   
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/barometer.png" alt="">
                             <p class="details-text">pression</p>
-                            <p class="thermometer-number">{{weather.main.pressure.toFixed()}}hPa</p>
+                            <p class="thermometer-number">{{weather.main.pressure.toFixed()}} hPa</p>
                         </div> 
                     </div>
+                    <h4 class="categories">Aurore et Crépuscule</h4>
                     <div class="details-sunrise-box">  
                         <div class="details-infos-box">
                             <img class="humidity-icon" src="../assets/weather-pack-icon/sunrise.png" alt="">
-                            <p class="details-text">levée</p>
-                            <p class="thermometer-number">{{weather.sys.sunrise}}%</p>
+                            <p class="details-text">levée (heure française)</p>
+                            <p class="thermometer-number">{{sunrise}}</p>
                         </div>   
                         <div class="details-infos-box">
                             <img class="thermometer-icon" src="../assets/weather-pack-icon/sunset.png" alt="">
-                            <p class="details-text">couchée</p>
-                            <p class="thermometer-number">{{weather.wind.speed}}km/h</p>
+                            <p class="details-text">couchée (heure française)</p>
+                            <p class="thermometer-number">{{sunset}}</p>
                         </div>   
                     </div>
                 </div>
@@ -102,6 +105,11 @@ var today = new Date();
 	var arr_day=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
 
 const currentDate = arr_day[dayOfTheWeek] + ', ' + day + ' ' + arr_month[month] + ' ' + year ; 
+
+
+// let sunriseConvertion = new Date(sunriseUnix*1000)
+// let sunrise = sunriseConvertion.getHours() + 'h' + sunriseConvertion.getMinutes()
+// console.log(sunrise)
 // console.log(currentDate, today);
 
 // let currentPosition = null;
@@ -139,17 +147,23 @@ export default {
             weather: undefined,
             api_code: '625ff0820b405788a4e383dacc79095a',
             url_recherche: 'https://api.openweathermap.org/data/2.5/weather?',
+            sunriseUnix: 0,
             sunrise: '',
+            sunsetUnix: 0,
+            sunset:'',
             
         }
     },
     created() {
         axios.get(`${this.url_recherche}q=nantes&units=metric&APPID=${this.api_code}&lang=fr`)
         .then(reponse => {
-                    console.log(reponse);
+                    // console.log(reponse);
                     this.weather=reponse.data;
                     this.iconCode=reponse.data.weather[0].main;
-                    console.log(this.iconUrl)
+                    this.sunriseUnix= new Date(reponse.data.sys.sunrise*1000);
+                    this.sunrise=this.sunriseUnix.getHours() + 'h' + this.sunriseUnix.getMinutes();
+                    this.sunsetUnix= new Date(reponse.data.sys.sunset*1000);
+                    this.sunset=this.sunsetUnix.getHours() + 'h' + this.sunsetUnix.getMinutes();
                 })
                 .finally(
                     this.requete='',
@@ -160,14 +174,24 @@ export default {
             if(event.key == "Enter"){
                 axios.get(`${this.url_recherche}q=${this.requete}&units=metric&APPID=${this.api_code}&lang=fr`)
                 .then(reponse => {
-                    console.log(reponse);
-                    this.weather=reponse.data
+                    // console.log(reponse);
+                    this.weather=reponse.data;
+                    this.sunriseUnix= new Date(reponse.data.sys.sunrise*1000);
+                    this.sunrise=this.sunriseUnix.getHours() + 'h' + this.sunriseUnix.getMinutes();
+                    this.sunsetUnix= new Date(reponse.data.sys.sunset*1000);
+                    this.sunset=this.sunsetUnix.getHours() + 'h' + this.sunsetUnix.getMinutes();
                 })
                 .finally(
                     this.requete=''
                 )
             }
+        },
+        sunriseFct(unix){
+            let sunriseConvertion = new Date(unix*1000);
+            let sunrise = sunriseConvertion.getHours() + 'h' + sunriseConvertion.getMinutes();
+            return(sunrise);
         }
+
     } 
 }
 
