ec1a9875218d3cbb99c449e551ecc3403d0e7467
ArnoSlb <soulabaille.arnaud@gmail.com>
2020-11-24 12:01:06 +0100

    mise en place des données dynamique, WIP sunrise et sunset

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,42 +1,88 @@
 <template>
     <div class="weather"> 
         <div class="container">
-            <h1>Hello Weather</h1>
+            <h1>Station Météo</h1>
             <div>
-            <label for="position">Search City</label>
+            <label for="position">Chercher une ville </label>
             <input 
                 id="position" 
                 type="text"
-                class=""
+                class="searching-city"
                 v-model="requete"
                 v-on:keypress="goWeather"
             >
                 <div>
-                    <h3>Position: Paris</h3>
-                    <div>
-                        <p>Temperature : 20°C</p>
-                        <p>Temps: Nuageux</p>
-                        
+                    <h3>Détails de la météo pour {{weather.name}}</h3>
+                    <h4 class=weather-description>{{weather.weather[0].description}}</h4>
+                    <div class="details-temp-box">
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
+                            <p class="details-text">min</p>
+                            <p class="thermometer-number">{{weather.main.temp_min.toFixed()}}°C</p>
+                        </div>
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
+                            <p class="thermometer-number">{{weather.main.temp.toFixed()}}°C</p>
+                        </div> 
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
+                            <p class="details-text">ressentis</p>
+                            <p class="thermometer-number">{{weather.main.feels_like.toFixed()}}°C</p>
+                        </div>  
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/024-thermometer-5.png" alt="">
+                            <p class="details-text">max</p>
+                            <p class="thermometer-number">{{weather.main.temp_max.toFixed()}}°C</p>
+                        </div>   
+                    </div>
+                    <div class="details-temp-box">  
+                        <div class="details-infos-box">
+                            <img class="humidity-icon" src="../assets/weather-pack-icon/humidity.png" alt="">
+                            <p class="details-text">humidité</p>
+                            <p class="thermometer-number">{{weather.main.humidity.toFixed()}}%</p>
+                        </div>   
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/027-wind-sign.png" alt="">
+                            <p class="details-text">vitesse</p>
+                            <p class="thermometer-number">{{weather.wind.speed}}km/h</p>
+                        </div>   
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/barometer.png" alt="">
+                            <p class="details-text">pression</p>
+                            <p class="thermometer-number">{{weather.main.pressure.toFixed()}}hPa</p>
+                        </div> 
+                    </div>
+                    <div class="details-sunrise-box">  
+                        <div class="details-infos-box">
+                            <img class="humidity-icon" src="../assets/weather-pack-icon/sunrise.png" alt="">
+                            <p class="details-text">levée</p>
+                            <p class="thermometer-number">{{weather.sys.sunrise}}%</p>
+                        </div>   
+                        <div class="details-infos-box">
+                            <img class="thermometer-icon" src="../assets/weather-pack-icon/sunset.png" alt="">
+                            <p class="details-text">couchée</p>
+                            <p class="thermometer-number">{{weather.wind.speed}}km/h</p>
+                        </div>   
                     </div>
                 </div>
             </div>
         </div> 
         <div class="title-infos">
             <div class="temperature">
-                <h1 class="title-temperature">16</h1>
+                <h1 class="title-temperature">{{weather.main.temp.toFixed()}}</h1>
                 <p class="temperature-logo">°</p>
             </div>
             <div class="date-city-icon">
                 <div class="date-time-infos">
-                    <h2 class="current-city">London</h2>
+                    <h2 class="current-city">{{weather.name}}</h2>
                     <div>
                         <p class="current-date">{{currentDate}}</p>
                     </div>
                 </div>
-                <div class="current-weather">
-                    <img class="icon-current-weather" src="../assets/weather-pack-icon/001-storm-12.png" alt="">
-                    <p class="text-current-weather">Storm</p>
-                </div>
+                <!-- <div class="current-weather">
+                    <img class="icon-current-weather" id="wicon" v-bind:src="iconUrl" alt="Weather icon">
+                    <p class="text-current-weather">{{weather.weather[0].description}}</p>
+                </div> -->
                 
             </div>
         </div>
@@ -50,14 +96,39 @@ import axios from 'axios';
 var today = new Date(); 
 	var year = today.getFullYear(); // retourne le millésime
     var month = today.getMonth(); // date.getMonth retourne un entier entre 0 et 11 
-    var arr_month=new Array("January", "February", "March", "April", "May", "June", "July","August","September","October","November","December");
+    var arr_month=new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet","Aout","Septembre","Octobre","Novembre","Decembre");
 	var day = today.getDate(); // retourne le jour (1à 31)
 	var dayOfTheWeek = today.getDay() ; // retourne un entier compris entre 0 et 6 (0 pour dimanche)
-	var arr_day=new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	var arr_day=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
 
 const currentDate = arr_day[dayOfTheWeek] + ', ' + day + ' ' + arr_month[month] + ' ' + year ; 
 // console.log(currentDate, today);
-        
+
+// let currentPosition = null;
+// console.log(currentPosition);
+
+// if (navigator.geolocation) {
+//     console.log('géolocalisation dispo');
+//     getLocation();
+// }
+
+// function getLocation() {
+//     navigator.geolocation.getCurrentPosition((position) => {
+//         // return the coordinates
+//         currentPosition = position.coords;
+//     }, (error) => {
+//         // check if the user denied geolocation, or if there was any other problem
+//         if (error.code == error.PERMISSION_DENIED) {
+//             alert('Geolocation has been disabled on this page, please review your browser\'s parameters');
+//         } else {
+//             alert('Unable to find your position, try again later.');
+//         }
+//     }, {
+//         timeout: 10000
+//     });
+// }
+
+
 
 export default {
     name: 'Weather',
@@ -67,13 +138,27 @@ export default {
             requete: '',
             weather: undefined,
             api_code: '625ff0820b405788a4e383dacc79095a',
-            url_recherche: 'https://api.openweathermap.org/data/2.5/weather?'
+            url_recherche: 'https://api.openweathermap.org/data/2.5/weather?',
+            sunrise: '',
+            
         }
+    },
+    created() {
+        axios.get(`${this.url_recherche}q=nantes&units=metric&APPID=${this.api_code}&lang=fr`)
+        .then(reponse => {
+                    console.log(reponse);
+                    this.weather=reponse.data;
+                    this.iconCode=reponse.data.weather[0].main;
+                    console.log(this.iconUrl)
+                })
+                .finally(
+                    this.requete='',
+                )
     },
     methods: {
         goWeather(event){
             if(event.key == "Enter"){
-                axios.get(`${this.url_recherche}q=${this.requete}&units=metric&APPID=${this.api_code}`)
+                axios.get(`${this.url_recherche}q=${this.requete}&units=metric&APPID=${this.api_code}&lang=fr`)
                 .then(reponse => {
                     console.log(reponse);
                     this.weather=reponse.data
@@ -83,7 +168,7 @@ export default {
                 )
             }
         }
-    }
+    } 
 }
 
 </script>
